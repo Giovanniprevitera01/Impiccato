f0327a9d1453d67b491f181d2da601d0a1d9252d
Graziano01 <graziano01castiglione@gmail.com>
2022-05-28 16:20:52 +0200

    import tkinter as tk import sqlite3
    app = tk.Tk()
    app.geometry('800x600')
    app.title('Menu v0.1')
    
    # Label
    label = tk.Label(app, text='Test', font=('Arial', 20))
    label.pack(side='top')
    
    # Button 1
    start_butt = tk.Button(app, text='Start', width=10, height=2)
    start_butt.pack(side='right', padx=10, pady=10)
    # Button 2
    start2_butt = tk.Button(app, text='Start2', width=10, height=2)
    start2_butt.pack(side='right', padx=10, pady=10)
    
    def Database():
        global conn, cursor
        # Connect database
        conn = sqlite3.connect("test.db")
        cursor = conn.cursor()
        # Query create table
        cursor.execute("CREATE TABLE IF NOT EXISTS 'user' (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT, password TEXT)")
    
    # Start menu
    app.mainloop()

--- db/dbase.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-def Database():
-    global conn, cursor
-    # Connect database
-    conn = sqlite3.connect("test.db")
-    cursor = conn.cursor()
-    # Query create table
-    cursor.execute("CREATE TABLE IF NOT EXISTS 'user' (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT, password TEXT)")
