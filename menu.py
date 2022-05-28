611d4d0f868b316940a27e31a86f21c57f146de2
Graziano01 <graziano01castiglione@gmail.com>
2022-05-28 15:07:11 +0200

    add: label

--- menu.py
+++ menu.py
@@ -4,6 +4,9 @@ app = tk.Tk()
 app.geometry('800x600')
 app.title('Menu v0.1')
 
+label = tk.Label(app, text='Test')
+label.pack(side='top')
+
 start_butt = tk.Button(app, text='Start', width=10, height=2)
 start_butt.pack(side='right', padx=10, pady=10)
 
