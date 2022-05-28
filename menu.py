54fe59d7f2db77d9c4897c42b760142a52f596f4
Graziano01 <graziano01castiglione@gmail.com>
2022-05-28 14:53:38 +0200

    add: test button

--- menu.py
+++ menu.py
@@ -4,4 +4,10 @@ app = tk.Tk()
 app.geometry('800x600')
 app.title('Menu v0.1')
 
+start_butt = tk.Button(app, text='Start', width=10, height=2)
+start_butt.pack(side='right', padx=10, pady=10)
+
+start2_butt = tk.Button(app, text='Start2', width=10, height=2)
+start2_butt.pack(side='right', padx=10, pady=10)
+
 app.mainloop()
