63e9edf0dde79113bf79756b997127c113991b92
Graziano01 <graziano01castiglione@gmail.com>
2022-05-28 15:28:09 +0200

    minor changes

--- menu.py
+++ menu.py
@@ -4,13 +4,16 @@ app = tk.Tk()
 app.geometry('800x600')
 app.title('Menu v0.1')
 
-label = tk.Label(app, text='Test')
+# Label
+label = tk.Label(app, text='Test', font=('Arial', 20))
 label.pack(side='top')
 
+# Button 1
 start_butt = tk.Button(app, text='Start', width=10, height=2)
 start_butt.pack(side='right', padx=10, pady=10)
-
+# Button 2
 start2_butt = tk.Button(app, text='Start2', width=10, height=2)
 start2_butt.pack(side='right', padx=10, pady=10)
 
+# Start menu
 app.mainloop()
