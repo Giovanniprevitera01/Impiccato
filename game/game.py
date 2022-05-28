781c06b54f958c574a5880e522b9e25b93d24056
Graziano01 <graziano01castiglione@gmail.com>
2022-05-29 01:08:15 +0200

    feat: add game functionality

--- game/game.py
+++ game/game.py
@@ -6,4 +6,25 @@ with open('wordlist.txt') as f:
         words.append(line.strip())
 
 word = random.choice(words)
-print(word)
+#print(word)
+
+def wordGuessed(letter_list):
+    print(' '.join(letter for letter in letter_list))
+
+letters_guessed = []
+for letter in word:
+    letters_guessed.append('_')
+print(letters_guessed)
+
+while True:
+    choise = input('Guess a letter --> ')
+    if choise in word:
+        for i, letter in enumerate(word):
+            if letter == choise:
+                letters_guessed[i] = choise
+    else:
+        print('Wrong!')
+
+    print(letters_guessed)
+    
+    wordGuessed(letters_guessed)
